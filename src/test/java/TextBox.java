f7e170f78c611c070715db96c8707de871259767
Anna Usolskaya <rakeya@yandex.ru>
2024-03-10 22:56:51 +0300

    1 part of test 2.0

--- src/test/java/TextBox.java
+++ src/test/java/TextBox.java
@@ -13,31 +13,29 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class TextBox {
     @BeforeAll
-    static void beforeAll(){
-        Configuration.browserSize="1920*1080";
-        Configuration.baseUrl="https://demoqa.com";
+    static void beforeAll() {
+        Configuration.browserSize = "1920*1080";
+        Configuration.baseUrl = "https://demoqa.com";
 
     }
+
     @Test
     void fillForTests() {
         File file = new File("src/test/resources/bio-photo.jpg");
-        Configuration.pageLoadStrategy= "eager" ;
+        Configuration.pageLoadStrategy = "eager";
         open("/automation-practice-form");
         $("#firstName").setValue("Anna");
         $("#lastName").setValue("Ivanova");
         $("#userEmail").setValue("111@mai.ru");
         $(byText("Female")).click();
-
         $("#userNumber").setValue("7777777777");
-
-
-      $("#dateOfBirthInput").click();
+        $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue(String.valueOf(3));
         $(".react-datepicker__year-select").selectOptionByValue(String.valueOf(1985));
         $(byText("22")).click();
         $("#subjectsInput").setValue("subjects");
         $(byText("Sports")).click();
-       $("#uploadPicture").uploadFromClasspath("");
+        $("#uploadPicture").uploadFromClasspath("");
         $("#currentAddress").setValue("Address1");
         $("#state").click();
         $("#react-select-3-option-1").click();
@@ -49,7 +47,7 @@ public class TextBox {
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-         $("#submit").click();
+        $("#submit").click();
 
 
     }
