87154138b612a0931faadc2eb1f87678608c1433
Anna Usolskaya <rakeya@yandex.ru>
2024-03-10 22:41:43 +0300

    1 part of test

---
+++ src/test/java/TextBox.java
@@ -0,0 +1,61 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
+import java.util.Date;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBox {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize="1920*1080";
+        Configuration.baseUrl="https://demoqa.com";
+
+    }
+    @Test
+    void fillForTests() {
+        File file = new File("src/test/resources/bio-photo.jpg");
+        Configuration.pageLoadStrategy= "eager" ;
+        open("/automation-practice-form");
+        $("#firstName").setValue("1111");
+        $("#lastName").setValue("1111");
+        $("#userEmail").setValue("111@mai.ru");
+        $(byText("Female")).click();
+
+        $("#userNumber").setValue("7777777777");
+
+
+      $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue(String.valueOf(3));
+        $(".react-datepicker__year-select").selectOptionByValue(String.valueOf(1985));
+        $(byText("22")).click();
+        $("#subjectsInput").setValue("fefefee");
+        $(byText("Sports")).click();
+       $("#uploadPicture").uploadFromClasspath("");
+        $("#currentAddress").setValue("Address1");
+        $("#state").click();
+        $("#react-select-3-option-1").click();
+        $("#city").click();
+        $("#react-select-4-option-1").click();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+         $("#submit").click();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
