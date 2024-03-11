173d21c43a1786d2568e6cd2ea6f73406e51a33a
Anna Usolskaya <rakeya@yandex.ru>
2024-03-11 17:37:10 +0300

    2 part of test Pass

--- src/test/java/TextBox.java
+++ src/test/java/TextBox.java
@@ -8,21 +8,21 @@ import java.util.Date;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBox {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920*1080";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
 
     }
 
     @Test
     void fillForTests() {
         File file = new File("src/test/resources/bio-photo.jpg");
-        Configuration.pageLoadStrategy = "eager";
+
         open("/automation-practice-form");
         $("#firstName").setValue("Anna");
         $("#lastName").setValue("Ivanova");
@@ -32,7 +32,7 @@ public class TextBox {
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOptionByValue(String.valueOf(3));
         $(".react-datepicker__year-select").selectOptionByValue(String.valueOf(1985));
-        $(byText("22")).click();
+        $(byText("8")).click();
         $("#subjectsInput").setValue("subjects");
         $(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("");
@@ -49,7 +49,16 @@ public class TextBox {
         }
         $("#submit").click();
 
-
+        $$("tr").get(1).shouldHave(text("Anna Ivanova"));
+        $$("tr").get(2).shouldHave(text("111@mai.ru"));
+        $$("tr").get(3).shouldHave(text("Female"));
+        $$("tr").get(4).shouldHave(text("7777777777"));
+        $$("tr").get(5).shouldHave(text("08 April,1985"));
+        $$("tr").get(6).shouldHave(text("subjects"));
+        $$("tr").get(7).shouldHave(text("Sports"));
+        $$("tr").get(8).shouldHave(text("test"));
+        $$("tr").get(9).shouldHave(text("Address1"));
+        $$("tr").get(10).shouldHave(text("Uttar Pradesh Lucknow"));
     }
 }
 
