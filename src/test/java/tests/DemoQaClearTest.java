5a28d0f126cd24422fbfb268b6e057e853d9c222
Anna Usolskaya <rakeya@yandex.ru>
2024-03-17 21:26:46 +0300

    Test add

--- src/test/java/tests/DemoQaClearTest.java
+++ src/test/java/tests/DemoQaClearTest.java
@@ -9,7 +9,7 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class DemoQaClear {
+public class DemoQaClearTest {
     @BeforeAll
     static void beforeAll() {
         //  Configuration.browserSize = "1920*1080";
